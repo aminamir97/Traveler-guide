822599c485b05e74e4e586313c8a6df4554269f1
amin alameer <amin.alameer9@gmail.com>
2020-02-26 15:18:18 +0300

    first commit

---
+++ app/src/main/java/com/example/travelerguide/Login.java
@@ -0,0 +1,189 @@
+package com.example.travelerguide;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.audiofx.Visualizer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Login extends AppCompatActivity {
+
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+
+
+    // TAG is for show some tag logs in LOG screen.
+    public static final String TAG = "MainActivity";
+
+    // Request sing in code. Could be anything as you required.
+    public static final int RequestSignInCode = 7;
+
+    // Firebase Auth Object.
+    public FirebaseAuth firebaseAuth;
+
+    // Google API Client object.
+    public GoogleApiClient googleApiClient;
+
+    // Google Sign In button .
+    com.google.android.gms.common.SignInButton signInButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        // get reference to 'users' node
+        mFirebaseDatabase = mFirebaseInstance.getReference();
+
+        String x = mFirebaseInstance.getReference("admins").child("malak").getPath().toString();
+
+        Toast.makeText(Login.this,x,Toast.LENGTH_LONG).show();
+
+
+        mFirebaseInstance.getReference("admins").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Toast.makeText(Login.this,dataSnapshot.getValue().toString(),Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
+
+        signInButton = (com.google.android.gms.common.SignInButton)findViewById(R.id.signin);
+
+        // Getting Firebase Auth Instance into firebaseAuth object.
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Creating and Configuring Google Sign In object.
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Creating and Configuring Google Api Client.
+        googleApiClient = new GoogleApiClient.Builder(Login.this)
+                .enableAutoManage(Login.this , new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                } /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+
+        // Adding Click listener to User Sign in Google button.
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                UserSignInMethod();
+
+            }
+        });
+
+
+
+
+
+    }
+    // Sign In function Starts From Here.
+    public void UserSignInMethod(){
+
+        // Passing Google Api Client into Intent.
+        Intent AuthIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+
+        startActivityForResult(AuthIntent, RequestSignInCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RequestSignInCode){
+
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if (googleSignInResult.isSuccess()){
+
+                GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount();
+
+                FirebaseUserAuth(googleSignInAccount);
+            }
+
+        }
+    }
+    public void FirebaseUserAuth(GoogleSignInAccount googleSignInAccount) {
+
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
+
+        Toast.makeText(Login.this,""+ authCredential.getProvider(),Toast.LENGTH_LONG).show();
+
+        firebaseAuth.signInWithCredential(authCredential)
+                .addOnCompleteListener(Login.this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task AuthResultTask) {
+
+                        if (AuthResultTask.isSuccessful()){
+
+                            // Getting Current Login user details.
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+                            // Hiding Login in button.
+                            signInButton.setVisibility(View.GONE);
+
+
+                            Toast.makeText(Login.this,firebaseUser.getDisplayName() +"\n"+firebaseUser.getPhoneNumber()+"\n"+firebaseAuth.getCurrentUser(),Toast.LENGTH_LONG).show();
+
+
+
+                            // Setting up name into TextView.
+                          //  LoginUserName.setText("NAME =  "+ firebaseUser.getDisplayName().toString());
+
+                            // Setting up Email into TextView.
+                           // LoginUserEmail.setText("Email =  "+ firebaseUser.getEmail().toString());
+
+                        }else {
+                            Toast.makeText(Login.this,"Something Went Wrong",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+}
